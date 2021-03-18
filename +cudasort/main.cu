6200014f8d5e0c05388ee75d410cc6e356a38c34
devairdarolt <devairdarolt@gmail.com>
2021-03-18 18:54:05 -0300

    New Commit

--- +cudasort/main.cu
+++ +cudasort/main.cu
@@ -40,9 +40,9 @@ __global__ void sem_nome (int *vet_d, int size) {
    /**
 		0 < i=0 < 10 .... 10 < i=1 < 20 .... 20 < i=2 < 30 ... 30 < i=3 < 40(*i)
    */
-   int sub_vet_desordenado[part];
+   int sub_vet_desordenado[4];
    //sub_vet_desordenado = (int)malloc(sizeof(int)*part);
-   for(k=i;k<(i*10);k++){
+   for(k=i;k<(i*4);k++){
 		k=k;   	
    		sub_vet_desordenado[k] = vet_d[k]; 
    }   
