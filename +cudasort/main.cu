2fefac0aff1137542f8d0eb89d97e349ac66a398
devairdarolt <devairdarolt@gmail.com>
2021-03-18 22:36:55 -0300

    New Commit

--- +cudasort/main.cu
+++ +cudasort/main.cu
@@ -9,10 +9,13 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // função executada na GPU
-__global__ void sort (int *vet_d, int size) {
+__global__ void GPU_sort (int *vet_d, int vet_size,int nthreads) {
+   
+
    int k = threadIdx.x;   
-   int part = size / 10; //== cada trede ordenara quatro posições do vetor[40]
    printf("Nucleo %d\n",k );
+   int part = vet_size / nthreads; //== cada trede ordenara quatro posições do vetor[40]
+   
    /**
 		0 < i=0 < 4 .... 4 < i=1 < 8 .... 8 < i=2 < 12 ... 12 < i=3 < 18
    */
@@ -20,7 +23,7 @@ __global__ void sort (int *vet_d, int size) {
    int b = k*part+part;
    int i=0,j=0;
    int min_idx=0,temp;
-   /*for(i=a;i<b;i++){
+   for(i=a;i<b;i++){
    		min_idx = i;
    		for(j=i+1;j<b;j++){
    			if(vet_d[j]<vet_d[min_idx]){
@@ -31,7 +34,14 @@ __global__ void sort (int *vet_d, int size) {
    		temp = vet_d[min_idx];
    		vet_d[min_idx] = vet_d[i];
    		vet_d[i] = temp;	
-   }*/
+   }
+   /*
+   for(i=a;i<b;i++){
+
+   		printf("v[%i]:%d\n",i,vet_d[i] );
+   }
+   printf("\n");
+   */
    
    
 }
@@ -41,70 +51,80 @@ __global__ void sort (int *vet_d, int size) {
 
 // função executada no HOST
 
-__host__ int *criar_vetor_desordenado(int *v,int size);
+__host__ int *criar_vetor_desordenado(int *v,int vet_size);
 
-__host__ void vet_imprimir(int *v,int size);
+__host__ void vet_imprimir(int *v,int vet_size);
 
 int main (int argc, char ** argv) {
 	int nthreads = 4;
 	int nblocos = 1;
+	int vet_size = 20;
 
-	int size = 40;
+
+	if (argc == 2) {
+		nthreads = atoi(argv[1]);
+		vet_size = atoi(argv[2]);
+	}else{
+		printf ("./main <nthreads> <vet_size>\n");
+		printf ("Caso não haja passagem de parâmetros, nthreads=4 e vet_size=20\n");
+	} 
+
 	//vetores do host	
 	int *vet_desordenado=NULL, *vet_ordenado=NULL;
-	vet_desordenado = criar_vetor_desordenado(vet_desordenado,size);//aloca vetor em host
-	cudaMallocHost((void **) &vet_ordenado, size*sizeof(int));
+	vet_desordenado = criar_vetor_desordenado(vet_desordenado,vet_size);//aloca vetor em host
+	cudaMallocHost((void **) &vet_ordenado, vet_size*sizeof(int));
 	printf("Vetor desordenado\n");
-	vet_imprimir(vet_desordenado,size); 
+	vet_imprimir(vet_desordenado,vet_size); 
 
-
-	printf("vetor parcialmente ordenado\n");
+	
 	int *dev_vet =NULL;
-	cudaMalloc((void**)&dev_vet,size * sizeof(int));// aloca vetor na memória global da placa
-	cudaMemcpy (dev_vet, vet_desordenado, size*sizeof(int), cudaMemcpyHostToDevice);
-	sort<<<1,10>>>(dev_vet, size);
-	//cudaMemcpy (vet_ordenado, dev_vet, size, cudaMemcpyDeviceToHost);
-	//vet_imprimir(vet_ordenado,size); 
+	cudaMalloc((void**)&dev_vet,vet_size * sizeof(int));// aloca vetor na memória global da placa
+	cudaMemcpy (dev_vet, vet_desordenado, vet_size*sizeof(int), cudaMemcpyHostToDevice);
+	//Cada CUDA core ordena uma partição de DEV_VET
+	GPU_sort<<<1,nthreads>>>(dev_vet, vet_size,nthreads);
+	
+	cudaDeviceSynchronize();
+	cudaMemcpy (vet_ordenado, dev_vet, vet_size*sizeof(int), cudaMemcpyDeviceToHost);
+	
+	printf("Vetor parcialmente ordenado\n");
+	vet_imprimir(vet_ordenado,vet_size); 
 
 
 	return 0;
 }
 
-__host__ int *criar_vetor_desordenado(int *v,int size){
+__host__ int *criar_vetor_desordenado(int *v,int vet_size){
 
 	if(v!=NULL){
 		printf("O vetor informado ja existe!\n");
 		return v;
 	}
-	if(size < 0){
+	if(vet_size < 0){
 		printf("O tamanho do vetor tem que ser maior que 0\n");
 	}
 	
 
-	cudaMallocHost((void **) &v, size*sizeof(int));
+	cudaMallocHost((void **) &v, vet_size*sizeof(int));
 	
 	//inicia valores do vetor desordenado
-	for(int i=0;i<size;i++){
-		v[i]= rand() % size;// (0 <= rand <= size)
+	for(int i=0;i<vet_size;i++){
+		v[i]= rand() % vet_size;// (0 <= rand <= vet_size)
 	}
 	return v;
 }
-__host__ void vet_imprimir(int *v,int size){
+__host__ void vet_imprimir(int *v,int vet_size){
 	if(v==NULL){
 		printf("O vetor informado é NULL!\n");
 		return;
 	}
-	if(size < 0){
+	if(vet_size < 0){
 		printf("O tamanho do vetor tem que ser maior que 0\n");
 		return;		
 	}
 
 	printf("\n");
 	printf("\n");
-	for(int i=0;i<size;i++){
-		if(i%4==0){
-			printf("\n");
-		}
+	for(int i=0;i<vet_size;i++){		
 		printf("%d\n",v[i]);		
 		
 	}
